2590165fd16caf1245efd9966317d45250be9f1a
3sunshine3 <2676405606@qq.com>
2019-03-05 13:45:42 +0800

    李四修改的
    李四修改的

--- src/main/java/com/neusoft/App.java
+++ src/main/java/com/neusoft/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!66　张三在本地修改的" );
+        System.out.println( "李四在远程仓库直接修改的！！！" );
     }
 }
