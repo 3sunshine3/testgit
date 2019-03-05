b9ecde605215a5e8dc9b42c2da1e4767f0fa4d63
‘hljojc’ <745523011@qq.com>
2019-03-06 02:41:21 +0800

    黄李洁再次修改

--- src/main/java/com/neusoft/User.java
+++ src/main/java/com/neusoft/User.java
@@ -4,6 +4,6 @@ public class User {
 
     public static void main(String[] args) {
         System.out.println("User文件，，，");
-        System.out.println("黄李洁");
+        System.out.println("黄李洁二次修改");
     }
 }
