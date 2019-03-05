a9bc6ac63dce75488890cc8d745a303a1773aaff
‘3sunshine3’ <2676405606@qq.com>
2019-03-05 13:42:18 +0800

    张三和李四同时进行修改2

--- src/main/java/com/neusoft/App.java
+++ src/main/java/com/neusoft/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!66　张三在本地修改的" );
+        System.out.println( "Hello World!张三在本地修改的" );
     }
 }
